0c32cb4245f8c40c338dbc72c53f4d73f17eb7ed
Blake Needleman <blake.needleman@gmail.com>
2016-11-02 15:39:26 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -36,6 +36,17 @@ server.error(function(err, req, res, next){
                 },status: 500 });
     }
 });
+
+//DB connect
+var db;
+var MongoClient = mongo.MongoClient;
+MongoClient.connect('mongodb://quizzley:5hqbqzNv8a4bHOhkPsvdeOavgAHuKgIP2YBZAhBWOfoMGvF33SC7PBWjoApjZe7qSaLG6yC4Y9TLMB1lyi33oA==@quizzley.documents.azure.com:10250/quizzley?ssl=true', function(err, database) {
+    if(err) {
+        throw err;
+    }
+    db = database;
+});
+
 server.listen( port);
 
 //Setup Socket.IO
@@ -51,23 +62,6 @@ io.sockets.on('connection', function(socket){
     console.log('Client Disconnected.');
   });
 });
-
-//DB connect
-function getQuizzes() {
-    var MongoClient = mongo.MongoClient;
-    MongoClient.connect('mongodb://quizzley:5hqbqzNv8a4bHOhkPsvdeOavgAHuKgIP2YBZAhBWOfoMGvF33SC7PBWjoApjZe7qSaLG6yC4Y9TLMB1lyi33oA==@quizzley.documents.azure.com:10250/quizzley?ssl=true', function(err, db) {
-        if(err) {
-            throw err;
-        }
-        db.quizzes.find({}).toArray(function(err, docs) {
-            if(!err) {
-                db.close();
-                return docs;
-            }
-        });
-    });
-}
-
 
 ///////////////////////////////////////////
 //              Routes                   //
@@ -86,6 +80,47 @@ server.get('/', function(req,res){
   });
 });
 
+
+/*  "/contacts"
+ *    GET: finds all quizzes
+ *    POST: creates a new quiz
+ */
+app.get("/quizzes", function(req, res) {
+});
+
+app.post("/quizzes", function(req, res) {
+    var newQuiz = req.body;
+    var now = new Date();
+    newQuiz.createdDate = now;
+    newQuiz.lastModifiedDate = now;
+
+    if (!req.body.quizName) {
+        handleError(res, "Invalid user input", "Must provide a quiz name.", 400);
+    }
+
+    db.collection("quizzes").insertOne(newQuiz, function(err, doc) {
+        if (err) {
+            handleError(res, err.message, "Failed to create new contact.");
+        } else {
+            res.status(201).json(doc.ops[0]);
+        }
+    });
+});
+
+/*  "/quizzes/:id"
+ *    GET: find quiz by id
+ *    PUT: update quiz by id
+ *    DELETE: deletes quiz by id
+ */
+
+app.get("/quizzes/:id", function(req, res) {
+});
+
+app.put("/quizzes/:id", function(req, res) {
+});
+
+app.delete("/quizzes/:id", function(req, res) {
+});
 
 //A Route for Creating a 500 Error (Useful to keep around)
 server.get('/500', function(req, res){
